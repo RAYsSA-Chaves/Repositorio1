8ed20d08217536934cf31b8ad2c2c2573844d9b6
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-02-26 13:46:41 -0300

    Update Exercicios_Python.py

--- ExerciciosPython/Exercicios_Python.py
+++ ExerciciosPython/Exercicios_Python.py
@@ -90,9 +90,9 @@ nota1 = float(input("Digite a primeira nota "))
 nota2 = float(input("Digite a segunda nota "))
 nota3 = float(input("Digite a terceira nota "))
 
-peso1 = 5
-peso2 = 2
-peso3 = 3
+peso1 = float(input("Digite o peso da primeira nota "))
+peso2 = float(input("Digite o peso da segunda nota "))
+peso3 = float(input("Digite o peso da terceira nota "))
 
 Mp = ((nota1*peso1) + (nota2*peso2) + (nota3*peso3))/(peso1+peso2+peso3)
 
